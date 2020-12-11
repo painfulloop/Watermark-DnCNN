2cb51965a90f8a7aaa250a13e798e862510388d0
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2020-12-11 21:25:34 +0100

    modify number epochs and initial if

--- watermark_DnCNN_main.py
+++ watermark_DnCNN_main.py
@@ -6,13 +6,14 @@ import black_combine_test
 import cv2
 import utility
 
-key_imgs_path = utility.create_folder('key_imgs')
-trigger_img, verification_img = GeneratorTriggerVerificationImg(40, 40).generate_trigger_and_verification_img()
-cv2.imwrite(key_imgs_path + '/trigger_image.png', trigger_img)
-cv2.imwrite(key_imgs_path + '/verification_image.png', verification_img)
-data_process_dncnn.generate_patches()
-black_combine_train.train(8) #train model to make it watermarked
-Dip_train.train(2) #train auxiliary visualizer to associate verification img and copyright image
+if __name__ == '__main__':
+    key_imgs_path = utility.create_folder('key_imgs')
+    trigger_img, verification_img = GeneratorTriggerVerificationImg(40, 40).generate_trigger_and_verification_img()
+    cv2.imwrite(key_imgs_path + '/trigger_image.png', trigger_img)
+    cv2.imwrite(key_imgs_path + '/verification_image.png', verification_img)
+    data_process_dncnn.generate_patches()
+    black_combine_train.train(8) #train model to make it watermarked
+    Dip_train.train(8) #train auxiliary visualizer to associate verification img and copyright image
 
 
 
