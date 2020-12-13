adb74ab14db70bfaf052340cf2914a2cdb95065a
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2020-12-13 19:17:25 +0100

    add seed = None

--- key_generator.py
+++ key_generator.py
@@ -12,7 +12,9 @@ class GeneratorTriggerVerificationImg:
         self.n = n
 
     def generate_trigger_and_verification_img(self):
-        self.trigger_img = np.random.uniform(0, 255, (self.m, self.n))
+        np.random.seed(None)
+        self.trigger_img = np.random.random((self.m, self.n))*255
         filter_img = cv2.Sobel(self.trigger_img, cv2.CV_64F, 1, 1, ksize=3)
         self.verification_img = np.uint8(np.abs(filter_img))
         return self.trigger_img, self.verification_img
+
