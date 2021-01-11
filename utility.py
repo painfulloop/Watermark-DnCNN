1468ae97c22f4f6919713d679b570ed0f95758a2
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2021-01-11 15:20:11 +0100

    resized show image

--- utility.py
+++ utility.py
@@ -4,7 +4,25 @@ import cv2
 import numpy as np
 
 
+def ResizeWithAspectRatio(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    return cv2.resize(image, dim, interpolation=inter)
+
+
 def show_image(img, title="", wait=True):
+    if img.shape[0] > 1000:
+        img = ResizeWithAspectRatio(img, height=1000)
     cv2.imshow(title, img)
     if wait: cv2.waitKey(0)
 
